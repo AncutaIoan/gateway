946e9b91bc232f6ac9e47879e1c9540b05dd359c
eoancuta <eoancuta@gmail.com>
2025-03-15 23:50:42 +0200

    feat : added some route logic to familiarize with the concept

--- src/main/kotlin/adamicus/gateway/GatewayApplication.kt
+++ src/main/kotlin/adamicus/gateway/GatewayApplication.kt
@@ -2,10 +2,39 @@ package adamicus.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class GatewayApplication
 
 fun main(args: Array<String>) {
 	runApplication<GatewayApplication>(*args)
+}
+
+@Configuration
+class GatewayConfig {
+
+    @Bean
+    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+            .route { p ->
+                p.path("/get")
+                    .filters { f -> f.addRequestHeader("Hello", "World") }
+                    .uri("http://httpbin.org:80")
+            }
+            .route { p ->
+                p.host("*.circuitbreaker.com")
+                    .filters { f ->
+                        f.circuitBreaker { config ->
+                            config.setName("mycmd")
+                            config.setFallbackUri("forward:/fallback")
+                        }
+                    }
+                    .uri("http://httpbin.org:80")
+            }
+            .build()
+    }
 }
