b565593e020d609c5e9f7db12702873cef744753
eoancuta <eoancuta@gmail.com>
2025-03-22 18:41:55 +0200

    fix: moved routes to app.yaml

--- src/main/kotlin/adamicus/gateway/GatewayApplication.kt
+++ src/main/kotlin/adamicus/gateway/GatewayApplication.kt
@@ -1,49 +1,13 @@
 package adamicus.gateway
 
-import adamicus.gateway.filter.JwsValidationFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.cloud.gateway.route.RouteLocator
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class GatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatewayApplication>(*args)
-}
-
-@Configuration
-class GatewayConfig {
-
-    @Bean
-    fun myRoutes(builder: RouteLocatorBuilder, jwsValidationFilter: JwsValidationFilter): RouteLocator {
-        val filter = jwsValidationFilter.apply(JwsValidationFilter.Config())
-        return builder.routes()
-            // Define ping route first (no filter needed here)
-            .route("ping-route") { p ->
-                p.path("/ping")
-                    .and().method(HttpMethod.GET)
-                    .uri("http://localhost:8081")
-            }
-            // User service routes without filters
-            .route("user-service") { p ->
-                p.path("/api/auth/create", "/api/auth/authenticate")  // No filter
-                    .uri("http://localhost:8081")
-            }
-            // User service test route with JWS validation filter
-            .route("user-service") { p ->
-                p.path("/api/auth/test-endpoint")
-                    .and().method(HttpMethod.GET)
-                    .filters { f -> f.filter(filter) }
-                    .uri("http://localhost:8081")
-            }
-            .build()
-    }
-
+    runApplication<GatewayApplication>(*args)
 }
